400852059c96d1df37b26e7f8409715538872629
Ivan Lopatin <vaneklopatin.main@gmail.com>
2025-02-07 14:04:22 +0300

    Я думаю, что все работает

--- Perceptron/Perceptron.cs
+++ Perceptron/Perceptron.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
+﻿using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Perceptron
 {
@@ -20,73 +14,180 @@ namespace Perceptron
         Rhombus,
     }
 
-    public static class Perceptron
+    public class Perceptron
     {
 
         #region Параметры модели
         // Размер изображения
-        private static int Size { get; set; } = 100;
+        private int Size { get; set; } = 100;
         // Порог активации нейрона
-        private static int ActivationLimit { get; set; }
+        private int ActivationLimit { get; set; } = 3;
         // Изменение веса при коррекции
-        private static double WeightCorrection { get; set; }
+        private double WeightCorrection { get; set; } = 0.03;
         // Количество связей между рецептором и нейронами
-        private static int ConnectionsCount { get; set; }
+        private int ConnectionsCount { get; set; }
         #endregion
 
         #region Структуры данных
-        // Слой рецепторов
-        private static Bitmap ReceptorLayer { get; set; }
+
         // Связи между рецепторным и ассоциативным слоями
-        private static List<List<List<Coordinates>>> Connections { get; set; }
-        // Ассоциативный слой
-        private static List<List<int>> AssociationLayer { get; set; }
+        private  List<List<List<Coordinates>>> Connections { get; set; }
+
         // Веса
-        private static List<List<int>> Weights { get; set; }
+        private List<List<double>> Weights { get; set; }
         #endregion
 
         #region Параметры обучения
         // Успешные ответы
-        private static int Successes { get; set; } = 0;
+        private int Successes { get; set; } = 0;
         // Количество итераций
-        private static int StepCount { get; set; }
+        private int StepCount { get; set; } = 100000;
         // Текущая итерация
-        private static int CurrentStep { get; set; }
+        private int CurrentStep { get; set; }
         #endregion
         #region Служебные переменные
-        private static Random Random { get; set; } = new Random();
-        private static bool IsDebugging { get; set; }
+        private Random Random { get; set; } = new Random();
+        private bool IsDebugging { get; set; }
         #endregion
-
-
-        static Perceptron()
+        public Perceptron(int size = 20, int connectionsCount = 20, int activationLimit = 3)
         {
-            // Инициализация свойств, если необходимо
+            Size = size;
+            ConnectionsCount = connectionsCount;
+            ActivationLimit = activationLimit;
+
+            Weights = new List<List<double>>();
+            Connections = new List<List<List<Coordinates>>>();
+
+            for (var i = 0; i < Size; i++)
+            {
+                Weights.Add(new List<double>());
+                Connections.Add(new List<List<Coordinates>>());
+                for (var j = 0; j < Size; j++)
+                {
+                    Weights[i].Add(0.0);
+                    Connections[i].Add(new List<Coordinates>());
+                    for (var k = 0; k < ConnectionsCount; k++)
+                    {
+                        Connections[i][j].Add(new Coordinates());
+                    }
+                }
+            }
         }
 
-        private static bool Run(int connectionsCount)
+        public bool Run()
         {
             // TODO: засунуть все в фор по количеству итераций. Связи постоянны или меняются каждую итерацию? Очищать слои на каждой итерации.
             // Генерация случайных связей между рецепторами и нейронами
-            ConnectionsCount = connectionsCount;
             for (var x = 0; x < Size; x++)
             {
                 for (var y = 0; y < Size; y++)
                 {
-                    for (var k = 0; k < connectionsCount; k++)
+                    for (var k = 0; k < ConnectionsCount; k++)
                         Connections[x][y][k] = new Coordinates { X = Random.Next(Size), Y = Random.Next(Size) };
                 }
             }
 
+            for (var step = 1; step <= StepCount; step++)
+            {
+                // Генерируем случайчное изображение
+                var imageType = (step % 2) == 0 ? ImageType.Circle : ImageType.Rhombus;
+                var image = GenerateImage(imageType);
+
+                // Распознаем изображение
+                var recognizedImage = Recognize(image);
+
+                // Обучаем, если изображение не распознано
+                if (imageType != recognizedImage.Item1)
+                {
+                    for (var x = 0; x < Size; x++)
+                    {
+                        for (var y = 0; y < Size; y++)
+                        {
+                            if (recognizedImage.Item2[x][y] == 1)
+                            {
+                                if (imageType == ImageType.Circle)
+                                    Weights[x][y] += WeightCorrection;
+                                else
+                                    Weights[x][y] -= WeightCorrection;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Successes += 1;
+                }
+
+                if (step % 100 == 0)
+                {
+                    double percentOfSuccess = Successes * 100.0 / step;
+
+                    Console.WriteLine($"Шаг: {step} | Успешно: {percentOfSuccess}%");
+                }
+            }
 
             return true;
         }
-        public static Bitmap GenerateImage(ImageType imageType)
+
+        public (ImageType, List<List<int>>) Recognize(Bitmap image)
+        {
+            // Формируем ассоциативный слой
+            var associationLayer = new List<List<int>>();
+            for (var i = 0; i < Size; i++)
+            {
+                associationLayer.Add(new List<int>());
+                for (var j = 0; j < Size; j++)
+                {
+                    associationLayer[i].Add(0);
+                }
+            }
+
+            // Формируем входы ассоциативного слоя
+            for (var x = 0; x < Size; x++)
+            {
+                for (var y = 0; y < Size; y++)
+                {
+                    var pixel = image.GetPixel(x, y);
+                    if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                    {
+                        // Если пиксель черный, то активируем нейроны
+                        for (var k = 0; k < ConnectionsCount; k++)
+                        {
+                            var connection = Connections[x][y][k];
+                            associationLayer[connection.X][connection.Y] += 1;
+                        }
+                    }
+                }
+            }
+
+            // Формируем выходы ассоциативного слоя
+            for (var x = 0; x < Size; x++)
+            {
+                for (var y = 0; y < Size; y++)
+                {
+                    associationLayer[x][y] = associationLayer[x][y] > ActivationLimit ? 1 : 0;
+                }
+            }
+
+            // Отображение
+            var effectorLayer = 0.0;
+            for (var x = 0; x < Size; x++)
+            {
+                for (var y = 0; y < Size; y++)
+                {
+                    effectorLayer += associationLayer[x][y] * Weights[x][y];
+                }
+            }
+
+            var recognizedImage = (effectorLayer > 0 ? ImageType.Circle : ImageType.Rhombus, associationLayer);
+            return recognizedImage;
+        }
+        public Bitmap GenerateImage(ImageType imageType)
         {
             var image = new Bitmap(Size, Size);
             using (Graphics g = Graphics.FromImage(image))
             {
-                g.Clear(Color.Transparent);
+                g.Clear(Color.White);
                 switch (imageType)
                 {
                     case ImageType.Circle:
--- Perceptron/Program.cs
+++ Perceptron/Program.cs
@@ -1,3 +1,27 @@
-﻿using Perceptron;
+﻿using System.Drawing.Imaging;
+using Perceptron;
 
-Perceptron.Perceptron.GenerateImage(ImageType.Rhombus);
+Perceptron.Perceptron perceptron = new Perceptron.Perceptron();
+perceptron.Run();
+
+for (int i = 0; i < 1000; i++)
+{
+    var imageType = i % 2 == 0 ? ImageType.Circle : ImageType.Rhombus;
+    var image = perceptron.GenerateImage(imageType);
+    var result = perceptron.Recognize(image).Item1;
+
+    if (i >= 979)
+    {
+        Console.WriteLine($"Шаг: {i}");
+        for (int y = 0; y < image.Height; y++)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                var pixelColor = image.GetPixel(x, y);
+                Console.Write(pixelColor.R > 128 ? "█" : " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"Я думаю, что это {result}!\n======================");
+    }
+}
